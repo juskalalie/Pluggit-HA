f2ca3e61199f01b698187b508ed6fd28632c2959
Goemon64 <>
2024-11-20 18:01:18 +0100

    add new pypluggit

--- custom_components/pluggit/pypluggit/pluggit.py
+++ custom_components/pluggit/pypluggit/pluggit.py
@@ -5,7 +5,7 @@ from pymodbus.client import ModbusTcpClient
 from pymodbus.constants import Endian
 from pymodbus.payload import BinaryPayloadBuilder, BinaryPayloadDecoder
 
-from const import (
+from .const import (
     BYPASS_STATE,
     CURRENT_UNIT_MODE,
     DEGREE_OF_DIRTINESS,
@@ -26,21 +26,12 @@ class Pluggit:
         self.host = host
         self.client = ModbusTcpClient(host=self.host)
 
-    def __check_connection(self) -> bool:
-        if self.client.connected:
-            return True
-
-        if self.client.connect() is not True:
-            return False
-
     def __read_register(self, register: Registers):
-        if self.__check_connection() is False:
-            return None
-
         item = REGISTER_DIC[register]
 
         try:
-            result = self.client.read_holding_registers(address=item[0], count=2)
+            result = self.client.read_holding_registers(
+                address=item[0], count=2)
             decoder = BinaryPayloadDecoder.fromRegisters(
                 result.registers, byteorder=Endian.BIG, wordorder=Endian.LITTLE
             )
@@ -53,18 +44,17 @@ class Pluggit:
             return decoder.decode_32bit_float()
 
     def __write_register(self, register: Registers, data: int):
-        if self.__check_connection() is False:
-            return None
-
         item = REGISTER_DIC[register]
-        builder = BinaryPayloadBuilder(byteorder=Endian.BIG, wordorder=Endian.LITTLE)
+        builder = BinaryPayloadBuilder(
+            byteorder=Endian.BIG, wordorder=Endian.LITTLE)
 
         if item[1] == RegisterType.UINT_32:
             builder.add_32bit_uint(data)
         elif item[1] == RegisterType.FLOAT:
             builder.add_32bit_float(data)
 
-        self.client.write_registers(address=item[0], values=builder.to_registers())
+        self.client.write_registers(
+            address=item[0], values=builder.to_registers())
 
     def get_unit_type(self):
         ret = self.__read_register(register=Registers.PRM_SYSTEM_ID)
@@ -72,10 +62,12 @@ class Pluggit:
             return DEVICE_TYPE[(ret >> 24) & 0x0F]
 
     def get_serial_number(self):
-        low = self.__read_register(register=Registers.PRM_SYSTEM_SERIAL_NUM_LOW)
-        high = self.__read_register(register=Registers.PRM_SYSTEM_SERIAL_NUM_HIGH)
+        low = self.__read_register(
+            register=Registers.PRM_SYSTEM_SERIAL_NUM_LOW)
+        high = self.__read_register(
+            register=Registers.PRM_SYSTEM_SERIAL_NUM_HIGH)
 
-        if low or high is not None:
+        if low and high is not None:
             return (high << 32) + low
 
     def get_firmware_version(self):
@@ -112,7 +104,8 @@ class Pluggit:
         return self.__read_register(register=Registers.PRM_FILTER_REMAINING_TIME)
 
     def get_filter_dirtiness(self):
-        dirt = self.__read_register(register=Registers.PRM_FILTER_DIRTINESS_DEGREE)
+        dirt = self.__read_register(
+            register=Registers.PRM_FILTER_DIRTINESS_DEGREE)
 
         if dirt is not None:
             return DEGREE_OF_DIRTINESS[dirt]
@@ -130,7 +123,8 @@ class Pluggit:
         return self.__read_register(register=Registers.PRM_BYPASS_TMAX_SUMMER)
 
     def get_bypass_actual_state(self):
-        state = self.__read_register(register=Registers.PRM_RAM_IDX_BYPASS_ACTUAL_STATE)
+        state = self.__read_register(
+            register=Registers.PRM_RAM_IDX_BYPASS_ACTUAL_STATE)
 
         if state is not None:
             return BYPASS_STATE[state]
@@ -141,14 +135,14 @@ class Pluggit:
         )
 
     def set_date_time(self):
-        # self.__write_register(register=Registers.PRM_DATE_TIME_SET, data=1731618033)
         self.__write_register(
             register=Registers.PRM_DATE_TIME_SET,
             data=int(time.time()) + (time.timezone * (-1)),
         )
 
     def set_unit_mode(self, mode: ActiveUnitMode):
-        self.__write_register(register=Registers.PRM_RAM_IDX_UNIT_MODE, data=mode.value)
+        self.__write_register(
+            register=Registers.PRM_RAM_IDX_UNIT_MODE, data=mode.value)
 
     def set_speed_level(self, speed: SpeedLevelFan):
         self.__write_register(
@@ -156,7 +150,8 @@ class Pluggit:
         )
 
     def set_default_filter_time(self, time: int):
-        self.__write_register(register=Registers.PRM_FILTER_DEFAULT_TIME, data=time)
+        self.__write_register(
+            register=Registers.PRM_FILTER_DEFAULT_TIME, data=time)
 
     def reset_filter(self):
         self.__write_register(register=Registers.PRM_FILTER_RESET, data=1)
@@ -168,10 +163,12 @@ class Pluggit:
         self.__write_register(register=Registers.PRM_BYPASS_TMAX, data=temp)
 
     def set_bypass_tmin_summer(self, temp: float):
-        self.__write_register(register=Registers.PRM_BYPASS_TMIN_SUMMER, data=temp)
+        self.__write_register(
+            register=Registers.PRM_BYPASS_TMIN_SUMMER, data=temp)
 
     def set_bypass_tmax_summer(self, temp: float):
-        self.__write_register(register=Registers.PRM_BYPASS_TMAX_SUMMER, data=temp)
+        self.__write_register(
+            register=Registers.PRM_BYPASS_TMAX_SUMMER, data=temp)
 
     def set_bypass_manual_timeout(self, timeout: int):
         self.__write_register(
